48d39251edcd60c78063029294810ac6f5f9ccf6
KDanielRosas <k.daniel.rosas99@gmail.com>
2023-03-21 17:24:34 -0600

    Add project files.

---
+++ BL/Paciente.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ML;
+using System.Runtime.InteropServices;
+
+namespace BL
+{
+    public class Paciente
+    {
+        public static ML.Result Add(ML.Paciente paciente)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string query = "PacienteAdd";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Connection = context;
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        SqlParameter[] parametros = new SqlParameter[7];
+
+                        parametros[0] = new SqlParameter("Nombre", SqlDbType.VarChar);
+                        parametros[0].Value = paciente.Nombre;
+
+                        parametros[1] = new SqlParameter("ApellidoPaterno", SqlDbType.VarChar);
+                        parametros[1].Value = paciente.ApellidoPaterno;
+
+                        parametros[2] = new SqlParameter("ApellidoMaterno", SqlDbType.VarChar);
+                        parametros[2].Value = paciente.ApellidoMaterno;
+
+                        parametros[3] = new SqlParameter("FechaNacimiento", SqlDbType.VarChar);
+                        parametros[3].Value = paciente.FechaNacimiento;
+
+                        parametros[4] = new SqlParameter("IdTipoSangre", SqlDbType.VarChar);
+                        parametros[4].Value = paciente.TipoSangre.IdTipoSangre;
+
+                        parametros[5] = new SqlParameter("Sexo", SqlDbType.Char);
+                        parametros[5].Value = paciente.Sexo;
+
+                        parametros[6] = new SqlParameter("Diagnostico", SqlDbType.VarChar);
+                        parametros[6].Value = paciente.Diagnostisco;
+
+                        cmd.Parameters.AddRange(parametros);
+                        cmd.Connection.Open();
+
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        if (filasAfectadas > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                        }
+
+                    }//SQLCommand
+                }//SQLConnection
+            }//try
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Exception = ex;
+                throw;
+            }//catch                     
+            return result;
+        }//Add
+
+        public static ML.Result Update(ML.Paciente paciente)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string query = "PacienteUpdate";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Connection = context;
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        SqlParameter[] parametros = new SqlParameter[8];
+
+                        parametros[0] = new SqlParameter("IdPaciente", SqlDbType.Int);
+                        parametros[0].Value = paciente.IdPaciente;
+
+                        parametros[1] = new SqlParameter("Nombre", SqlDbType.VarChar);
+                        parametros[1].Value = paciente.Nombre;
+
+                        parametros[2] = new SqlParameter("ApellidoPaterno", SqlDbType.VarChar);
+                        parametros[2].Value = paciente.ApellidoPaterno;
+
+                        parametros[3] = new SqlParameter("ApellidoMaterno", SqlDbType.VarChar);
+                        parametros[3].Value = paciente.ApellidoMaterno;
+
+                        parametros[4] = new SqlParameter("FechaNacimiento", SqlDbType.VarChar);
+                        parametros[4].Value = paciente.FechaNacimiento;
+
+                        parametros[5] = new SqlParameter("IdTipoSangre", SqlDbType.VarChar);
+                        parametros[5].Value = paciente.TipoSangre.IdTipoSangre;
+
+                        parametros[6] = new SqlParameter("Sexo", SqlDbType.Char);
+                        parametros[6].Value = paciente.Sexo;
+
+                        parametros[7] = new SqlParameter("Diagnostico", SqlDbType.VarChar);
+                        parametros[7].Value = paciente.Diagnostisco;
+
+                        cmd.Parameters.AddRange(parametros);
+                        cmd.Connection.Open();
+
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        if (filasAfectadas > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                        }
+
+                    }//SQLCommand
+                }//SQLConnection
+            }//try
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Exception = ex;
+                throw;
+            }//catch                     
+            return result;
+        }//Add
+        public static ML.Result Delete(int idPaciente)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string query = "PacienteDelete";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = context;
+
+                        SqlParameter[] parametros = new SqlParameter[1];
+
+                        parametros[0] = new SqlParameter("IdPaciente", SqlDbType.Int);
+                        parametros[0].Value = idPaciente;
+
+                        cmd.Parameters.AddRange(parametros);
+                        cmd.Connection.Open();
+
+                        int filasAfectadas = cmd.ExecuteNonQuery();
+
+                        if (filasAfectadas > 0)
+                        {
+                            result.Correct = true;
+                        }
+                        else
+                        {
+                            result.Correct = false;
+                        }
+                    }
+                }
+            }//try
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                throw;
+            }//catch
+            return result;
+        }//Delete
+
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string query = "PacienteGetAll";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Connection = context;
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        DataTable dataTable = new DataTable();
+
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                        sqlDataAdapter.Fill(dataTable);
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            result.Objects = new List<object>();
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                ML.Paciente paciente = new ML.Paciente();
+                                paciente.IdPaciente = (int)row[0];
+                                paciente.Nombre = (string)row[1];
+                                paciente.ApellidoPaterno = (string)row[2];
+                                paciente.ApellidoMaterno = (string)row[3];
+                                paciente.FechaNacimiento = (string)row[4];
+                                paciente.TipoSangre = new TipoSangre();
+                                paciente.TipoSangre.IdTipoSangre = (byte)row[5];
+                                paciente.TipoSangre.Nombre = (string)row[6];
+                                paciente.Sexo = (string)row[7];
+                                paciente.FechaIngreso = (DateTime)row[8];
+                                paciente.Diagnostisco = (string)row[9];
+
+                                result.Objects.Add(paciente);
+                            }//foreach
+                        }//if
+                        result.Correct = true;
+                    }//SQLCommand
+                }//SQLConnection
+            }//try
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Exception = ex;
+                throw;
+            }//catch
+            return result;
+        }//GetAll}
+
+        public static ML.Result GetById(int idPaciente)
+        {
+            ML.Result result = new Result();
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnection()))
+                {
+                    string query = "PacienteGetById";
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Connection = context;
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        SqlParameter[] parametros = new SqlParameter[1];
+
+                        parametros[0] = new SqlParameter("IdPaciente", SqlDbType.Int);
+                        parametros[0].Value = idPaciente;
+
+                        cmd.Parameters.AddRange(parametros);
+                        cmd.Connection.Open();
+
+                        DataTable dataTable = new DataTable();
+
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                        sqlDataAdapter.Fill(dataTable);
+
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            result.Object = new ML.Paciente();
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                ML.Paciente paciente = new ML.Paciente();
+                                paciente.IdPaciente = (int)row[0];
+                                paciente.Nombre = (string)row[1];
+                                paciente.ApellidoPaterno = (string)row[2];
+                                paciente.ApellidoMaterno = (string)row[3];
+                                paciente.FechaNacimiento = (string)row[4];
+                                paciente.TipoSangre = new TipoSangre();
+                                paciente.TipoSangre.IdTipoSangre = (byte)row[5];
+                                paciente.TipoSangre.Nombre = (string)row[6];
+                                paciente.Sexo = (string)row[7];
+                                paciente.FechaIngreso = (DateTime)row[8];
+                                paciente.Diagnostisco = (string)row[9];
+
+                                result.Object = paciente;
+                            }//foreach
+                        }//if
+                        result.Correct = true;
+                    }//SQLCommand
+                }//SQLConnection
+            }//try
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Exception = ex;
+                throw;
+            }//catch
+            return result;
+        }//GetAll
+    }//classPaciente
+}//NS
---
+++ PL/Program.cs
@@ -0,0 +1,172 @@
+﻿using ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t----- Bienvenido -----");
+            int op = 0;
+            
+            while (op != 6)
+            {
+                
+                Console.WriteLine("\n¿Qué acción desea realizar?" +
+                                  "\n 1. Agregar un nuevo paciente \n 2. Actualizar registro de paciente " +
+                                  "\n 3. Eliminar un registro de paciente \n 4. Mostrar todos los pacientes " +
+                                  "\n 5. Buscar paciente por su ID \n 6. Salir");
+                Console.Write("Su opción: ");
+                op = int.Parse(Console.ReadLine());
+                
+                switch (op)
+                {
+
+                    case 1:
+                        ML.Result result = Add();
+                        if (result.Correct)
+                        {
+                            Console.WriteLine("\n\t----- Paciente registrado correctamente -----");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error al registar al paciente");
+                        }
+                        break;
+
+                    case 2:
+                        result = Update();
+                        if (result.Correct)
+                        {
+                            Console.WriteLine("\n\t----- Registro actualizado correctamente -----");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error al actualizar al paciente");
+                        }
+                        break;
+
+                    case 3:
+                        Console.Write("\nIngrese el ID del registro a eliminar: ");
+                        int id = int.Parse(Console.ReadLine());
+                        result = BL.Paciente.Delete(id);
+                        if (result.Correct)
+                        {
+                            Console.WriteLine("\n\t----- Registro eliminado correctamente ------");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\t ----- Error al eliminar el registro -----");
+                        }
+                        break;
+
+                    case 4:
+                        result = BL.Paciente.GetAll();
+                        if (result.Correct)
+                        {
+                            Console.WriteLine("\t----- Pacientes -----");
+                            foreach (ML.Paciente paciente in result.Objects.Cast<Paciente>())
+                            {                                
+                                Console.Write("Id: " + paciente.IdPaciente);
+                                Console.Write("\nNombre: " + paciente.Nombre);
+                                Console.Write("\nApellido Paterno: " + paciente.ApellidoPaterno);
+                                Console.Write("\nApellido Materno: " + paciente.ApellidoMaterno);
+                                Console.Write("\nFecha de Nacimiento: " + paciente.FechaNacimiento);
+                                Console.Write("\nIdTipoSangre: " + paciente.TipoSangre.IdTipoSangre);
+                                Console.Write("\nNombreTipoSangre: " + paciente.TipoSangre.Nombre);
+                                Console.Write("\nSexo: " + paciente.Sexo);
+                                Console.Write("\nFecha de Ingreso: " + paciente.FechaIngreso);
+                                Console.Write("\nDiagnostico: " + paciente.Diagnostisco);
+                                Console.WriteLine("\n--------------------");
+                            }
+                        }
+                        break;
+
+                    case 5:
+                        Console.Write("\nIngrese el ID del paciente: ");
+                        int idPaciente = int.Parse(Console.ReadLine());
+                        result = BL.Paciente.GetById(idPaciente);
+                        var item = (ML.Paciente)result.Object;
+                        if (result.Correct)
+                        {
+                            Console.WriteLine("\n\t----- Paciente -----");
+                                                       
+                                Console.Write("Id: " + item.IdPaciente);
+                                Console.Write("\nNombre: " + item.Nombre);
+                                Console.Write("\nApellido Paterno: " + item.ApellidoPaterno);
+                                Console.Write("\nApellido Materno: " + item.ApellidoMaterno);
+                                Console.Write("\nFecha de Nacimiento: " + item.FechaNacimiento);
+                                Console.Write("\nIdTipoSangre: " + item.TipoSangre.IdTipoSangre);
+                                Console.Write("\nNombreTipoSangre: " + item.TipoSangre.Nombre);
+                                Console.Write("\nSexo: " + item.Sexo);
+                                Console.Write("\nFecha de Ingreso: " + item.FechaIngreso);
+                                Console.Write("\nDiagnostico: " + item.Diagnostisco);
+                                Console.WriteLine("\n--------------------");
+                            
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            
+        }
+
+        static ML.Result Add()
+        {
+            ML.Paciente paciente = new ML.Paciente();
+            Console.WriteLine("Ingrese los datos del paciente: ");
+            Console.Write("Nombre: ");
+            paciente.Nombre = Console.ReadLine();
+            Console.Write("Apellido Paterno: ");
+            paciente.ApellidoPaterno = Console.ReadLine();
+            Console.Write("Apellido Materno: ");
+            paciente.ApellidoMaterno = Console.ReadLine();
+            Console.Write("Fecha de Nacimiento(DD-MM-AAAA): ");
+            paciente.FechaNacimiento = Console.ReadLine();
+            Console.Write("IdTipoSangre: ");
+            paciente.TipoSangre = new ML.TipoSangre();
+            paciente.TipoSangre.IdTipoSangre = int.Parse(Console.ReadLine());
+            Console.Write("Sexo: ");
+            paciente.Sexo = Console.ReadLine();
+            Console.Write("Diagnostico: ");
+            paciente.Diagnostisco = Console.ReadLine();
+
+            ML.Result result = BL.Paciente.Add(paciente);
+            return result;
+        }//Add
+
+        static ML.Result Update()
+        {
+            ML.Paciente paciente = new ML.Paciente();
+            Console.Write("\nIngrese el ID del registro a actualizar: ");
+            paciente.IdPaciente = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese los datos del paciente: ");
+            Console.Write("Nombre: ");
+            paciente.Nombre = Console.ReadLine();
+            Console.Write("Apellido Paterno: ");
+            paciente.ApellidoPaterno = Console.ReadLine();
+            Console.Write("Apellido Materno: ");
+            paciente.ApellidoMaterno = Console.ReadLine();
+            Console.Write("Fecha de Nacimiento(DD-MM-AAAA): ");
+            paciente.FechaNacimiento = Console.ReadLine();
+            Console.Write("IdTipoSangre: ");
+            paciente.TipoSangre = new ML.TipoSangre();
+            paciente.TipoSangre.IdTipoSangre = int.Parse(Console.ReadLine());
+            Console.Write("Sexo: ");
+            paciente.Sexo = Console.ReadLine();
+            Console.Write("Diagnostico: ");
+            paciente.Diagnostisco = Console.ReadLine();
+
+            ML.Result result = BL.Paciente.Update(paciente);
+            return result;            
+        }
+
+    }
+}
